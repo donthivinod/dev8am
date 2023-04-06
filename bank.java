0090586a42e73cff5fd8544197fbe2522b1fc3f6
donthivinod <donthivinod99@gmail.com>
2023-04-06 04:49:27 +0000

    bank code

---
+++ bank.java
@@ -0,0 +1,12 @@
+void main()
+{
+	printf("withdraw from a bank A code");
+}
+void main()
+{
+        printf("deposit from a bank B code");
+}
+void main()
+{
+        printf("11111111 withdraw 1111111111");
+}
